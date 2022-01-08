885c892663c892dbb1e82c4e46c0fc34e9918373
something411 <54055822+something411@users.noreply.github.com>
2022-01-08 15:22:51 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NFC Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('NFC Test'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
