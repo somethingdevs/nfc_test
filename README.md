cf7457ac1350b7a75ec6632f58712dc9614ad1c8
something411 <54055822+something411@users.noreply.github.com>
2022-01-12 19:31:03 +0530

    first update
    nfc works but not able to read properly

--- README.md
+++ README.md
@@ -2,13 +2,8 @@
 
 Testing out NFC with Flutter
 
-# Pakcages to use
+# Pakcages using
 
 nfc_manager: ^3.1.0 - https://pub.dev/packages/nfc_manager
     
-
-nfc_in_flutter: ^2.0.5 - https://pub.dev/packages/nfc_in_flutter
-
-    ⚠️ Currently only NDEF formatted tags are supported.
-
 flutter_nfc_kit: ^3.2.0 - https://pub.dev/packages/flutter_nfc_kit
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
+var result;
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+void _tagRead() {
+  NfcManager.instance.startSession(
+    onDiscovered: (tag) async {
+      result.value = tag.data;
+      print(result.toString());
+      NfcManager.instance.stopSession();
+    },
+  );
+}
+
+void _ndefWrite() {
+  NfcManager.instance.startSession(
+    onDiscovered: (tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = 'Tag is not NDEF writable';
+        NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+      NdefMessage message = NdefMessage(
+        [
+          NdefRecord.createText('joe nuts'),
+        ],
+      );
+
+      //The actual writing part
+      try {
+        await ndef.write(message);
+        result.value = 'Sucess!';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        result.value = e;
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    },
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ValueNotifier<dynamic> result = ValueNotifier(null);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +66,21 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('NFC Test'),
         ),
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: _tagRead,
+                child: Text('Read Tag'),
+              ),
+              ElevatedButton(
+                onPressed: _ndefWrite,
+                child: Text('Write NDEF Tag'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
